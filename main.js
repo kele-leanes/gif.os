7142cae05cb08156686f9ca67c6a556e1cea5eda
Ezequiel Leanes <ezequiel.leanes@gmail.com>
2020-01-31 12:55:36 -0300

    totaly new stylesheets

--- main.js
+++ main.js
@@ -1,38 +1,27 @@
-let dropdown = document.querySelector(".dropdownWrapper");
+let dropdown = document.querySelector(".dropdown-wrapper");
 
 dropdown.addEventListener("click", toggleDropdown);
 
 function toggleDropdown(){
-    this.classList.toggle("active");
+    themeButtons.classList.toggle("active");
 }
 
-let themeButtons = document.querySelector(".dropdownMenu");
-let lightThemeBtn = document.querySelector(".lightThemeBtn");
-let darkThemeBtn = document.querySelector(".darkThemeBtn");
-let topWindow = document.getElementsByClassName("topWindow");
-let seeMore = document.getElementsByClassName("seeMore");
+let themeButtons = document.querySelector(".dropdown-menu");
+let lightThemeBtn = document.querySelector(".light-theme-btn");
+let darkThemeBtn = document.querySelector(".dark-theme-btn");
 
 themeButtons.addEventListener("click", toggleTheme);
 
 function toggleTheme(){
-    document.getElementsByTagName("body")[0].classList.toggle("night");
-    document.getElementsByTagName("header")[0].classList.toggle("night");
-    document.getElementsByClassName("navButton")[0].classList.toggle("night");
-    document.getElementsByClassName("navButton")[1].classList.toggle("night");
-    document.getElementsByClassName("dropdownBtn")[0].classList.toggle("night");
-    document.getElementsByClassName("topBar")[0].classList.toggle("night");
-    document.getElementsByClassName("myGif")[0].classList.toggle("night");
+    document.getElementsByTagName("body")[0].classList.toggle("dark");
     
-    for(var i = 0;i<topWindow.length;i++){
-        topWindow[i].classList.toggle("night");
-        seeMore[i].classList.toggle("night");
-    }
-
     if(lightThemeBtn.disabled == true){
         lightThemeBtn.disabled = false,darkThemeBtn.disabled = true;
         document.getElementsByClassName("logo-img")[0].src="/IMAGES/gifOF_logo_dark.png";
+        document.getElementById("search-icon").src="/IMAGES/Combined shape.svg"
     }else {
         lightThemeBtn.disabled = true,darkThemeBtn.disabled = false;
         document.getElementsByClassName("logo-img")[0].src="/IMAGES/gifOF_logo.png";
+        document.getElementById("search-icon").src="/IMAGES/lupa_inactive.svg"
     }
 }    
